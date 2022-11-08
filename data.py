8d0cd76095c074f9857e75c4c760ead9e9eeacee
amayuelas <aamayuelasfernandez@ucsb.edu>
2022-11-07 19:40:43 -0800

    untested training

--- data.py
+++ data.py
@@ -6,6 +6,8 @@ import os
 import json
 from tqdm import tqdm
 from pathlib import Path
+from torch.utils.data import Dataset
+import linecache
 
 from transformers import GPT2Tokenizer
 from datasets import load_dataset
@@ -27,7 +29,7 @@ def process_dataset(dataset_name="wmt14", dataset_languages="fr-en"):
         "gpt2",
         unk_token="<|unk|>",
         bos_token="<|bos|>",
-        eos_token="<|eos|>"
+        eos_token="<|eos|>", 
         )
 
     # Select from ["train", "validation", "test"]
@@ -79,8 +81,56 @@ def process_dataset(dataset_name="wmt14", dataset_languages="fr-en"):
         ensure_ascii=False
     )
         
-
     print("FINISHED!")
+
+def load_data(dataset_name, dataset_languages, split):
+    """
+    Loads the tokenized data
+    :param  dataset_name: name of the dataset [wmt14]
+    :param  dataset_languages: languages of the dataset [fr-en]
+    :param  split: selected split [train/validation/test]
+    :return: (source_sentences, target_sentences) tuple of source, target tokens
+    """
+    languages = dataset_languages.split('-')
+
+    filename_source = dataset_name + "_" + languages[1] + "_" + split + ".src"
+    filename_target = dataset_name + "_" + languages[0] + "_" + split + ".trg"
+
+    try:
+        with open(Path(DATASET_DIR, filename_source), 'r') as f_source:
+            source_sentences = [line for line in f_source]
+
+        with open(Path(DATASET_DIR, filename_target), 'r') as f_target:
+            target_sentences = [line for line in f_target] 
+
+    except FileNotFoundError:
+        print("Dataset file does not exist. Check parameter of load_data function.")
+
+    return source_sentences, target_sentences
+
+
+class Wmt14Dataset(Dataset):
+    def __init__(self, src_filename, trg_filename):
+
+        self.src_filename = src_filename
+        self.trg_filename = trg_filename
+        self.num_pairs = sum(1 for _ in open(Path(DATASET_DIR, src_filename), 'r'))
+
+    def __len__(self):
+        return self.num_pairs
+
+    def __getitem__(self, idx):
+        source_sentence = linecache.getline(str(Path(DATASET_DIR,self.src_filename)), idx)
+        source_sentence = source_sentence.split(' ')[:-1]
+        target_sentence = linecache.getline(str(Path(DATASET_DIR, self.src_filename)), idx)
+        target_sentence = target_sentence.split(' ')[:-1]
+        return source_sentence, target_sentence
+
+
+def load_vocabulary():
+    filename_vocabulary = "vocab.json"
+    vocabulary = json.load(open(filename_vocabulary, 'r'))
+    return vocabulary
 
 if __name__ == "__main__":
     process_dataset()
